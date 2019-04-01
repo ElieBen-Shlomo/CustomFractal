37fef9cf0bd40a5c72c02c15388a2840648465a4
Elie Ben-Shlomo <elieben-shlomo@Elies-MacBook-Pro.local>
2019-04-01 21:27:51 +0100

    readding colour scheme

--- src/main.hs
+++ src/main.hs
@@ -3,10 +3,8 @@ import Graphics.UI.GLUT
 import Graphics.UI.GLUT.Window
 import Data.IORef
 
-screenWidth = 700
-screenHeight = 700
-widthDensity = 700 :: GLfloat
-heightDensity = 700 :: GLfloat 
+widthDensity = 300:: GLfloat
+heightDensity = 300 :: GLfloat 
 
 data Complex = C Float Float deriving (Show,Eq)
 instance Num Complex where
@@ -25,18 +23,11 @@ functionIterates f c = iterate (\z -> f z c) c
 
 type TruthCondition = Complex -> Bool -- can I make Complex a generic type instead?
 firstIndexOfElementSatisfyingCondition :: [Complex] -> TruthCondition -> Int
-firstIndexOfElementSatisfyingCondition (x:xs) condition = if condition x then 1 else firstIndexOfElementSatisfyingCondition xs condition
-
--- firstIndexOfElementSatisfyingCondition :: [Complex] -> TruthCondition -> Int -> Int
--- firstIndexOfElementSatisfyingCondition list condition n = snd $ headOrMax $ filteredList 
---     where
---     filteredList = filter (\(z, n) -> condition z) enumeratedList
---         where
---         enumeratedList = take n $ zip list [1,2..]
-        
+firstIndexOfElementSatisfyingCondition [] _ = 0
+firstIndexOfElementSatisfyingCondition (x:xs) condition = if condition x then 1 else 1 + firstIndexOfElementSatisfyingCondition xs condition
 
 numberOfIterations :: CustomFunction -> Complex -> TruthCondition -> Int -> Int
-numberOfIterations f z condition maxIterations = firstIndexOfElementSatisfyingCondition (functionIterates f z) condition maxIterations
+numberOfIterations f z condition maxIterations = firstIndexOfElementSatisfyingCondition (take maxIterations $ functionIterates f z) condition
 
 iteratedFunction :: Complex -> Complex -> CustomFunction -> Int -> Int
 iteratedFunction z c f n = if (magnitude z > 2 )
@@ -50,20 +41,18 @@ iterations x y n = numberOfIterations f (C real imag) condition n
                         real = 2.0 * x / widthDensity
                         imag = 2.0 * y / heightDensity
 
-factor = 0.5
-shiftX = -0.5
-shiftY = -0.5
+factor = 1.0
+shiftX = -0.0
+shiftY = -0.0
 colouredPoints :: [(GLfloat,GLfloat,Color3 GLfloat)]
-colouredPoints = [ (shiftX +factor*x/widthDensity, shiftY + factor*y/heightDensity, intToColour $ iterations x y 1)|
+colouredPoints = [ (shiftX +factor*x/widthDensity, shiftY + factor*y/heightDensity, intToColour $ iterations x y 100)|
                     x <- [-widthDensity..widthDensity],
                     y <- [-heightDensity..heightDensity]]
-                    where intToColour x =
-                            let
-                                colour :: GLfloat -> GLfloat    
-                                colour x =  x 
-                            in
-                                if x >= 50 then Color3 (colour 0) (colour 0) (colour 0) else Color3 (colour 100) (colour 50) (colour 200)
-                                --Color3 (colour x) (colour 20) (colour 50)
+                        where 
+                        intToColour n = Color3 (colour n) (colour 1) (colour 1)
+                            where 
+                                colour :: Int -> GLfloat    
+                                colour n = fromIntegral n/50
 
 condition :: TruthCondition                
 condition = (\z -> (magnitude z) > 2)  
@@ -74,7 +63,6 @@ main = do
   (progname,_) <- getArgsAndInitialize
   initialDisplayMode $= [DoubleBuffered]
   createWindow "Haskelbrot"
-  windowSize $= Size screenWidth screenHeight
   displayCallback $= display
   mainLoop where
     display = do
