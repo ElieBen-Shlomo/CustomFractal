82b649ef52741a6648416caa2cd66a026285960d
Elie Ben-Shlomo <elieben-shlomo@Elies-MacBook-Pro.local>
2019-04-04 21:13:08 +0100

    Seperating IO and fractal logic into different files

---
+++ src/fractal.hs
@@ -0,0 +1,44 @@
+module Fractal where
+
+    import Graphics.Rendering.OpenGL
+    import Graphics.UI.GLUT
+    import Graphics.UI.GLUT.Window
+
+    data Complex = C Float Float deriving (Show,Eq)
+    instance Num Complex where
+        fromInteger n = C (fromIntegral n) 0.0
+        (C x y) * (C z t) = C (z*x - y*t) (y*z + x*t)
+        (C x y) + (C z t) = C (x+z) (y+t)
+    
+    magnitude :: Complex -> Float
+    magnitude (C x y) = sqrt(x * x + y * y)
+    
+    type CustomFunction = Complex -> Complex -> Complex
+    functionIterates :: CustomFunction -> Complex -> [Complex]
+    functionIterates f c = iterate (\z -> f z c) c
+    
+    type TruthCondition = Complex -> Bool 
+    firstIndexOfElementSatisfyingCondition :: [Complex] -> TruthCondition -> Int
+    firstIndexOfElementSatisfyingCondition [] _ = 0
+    firstIndexOfElementSatisfyingCondition (x:xs) condition = if condition x then 1 else 1 + firstIndexOfElementSatisfyingCondition xs condition
+    
+    numberOfIterations :: CustomFunction -> Complex -> TruthCondition -> Int -> Int
+    numberOfIterations f z condition maxIterations = firstIndexOfElementSatisfyingCondition (take maxIterations $ functionIterates f z) condition
+    
+    iterations :: CustomFunction -> Float -> Float -> Int -> (GLfloat, GLfloat) -> Int          
+    iterations f x y n (widthDensity, heightDensity) = numberOfIterations f (C real imag) condition n
+                        where 
+                            condition z = (magnitude z) > 2
+                            real = 2.0 * x / widthDensity
+                            imag = 2.0 * y / heightDensity
+                            
+    colouredPoints :: CustomFunction -> (Float, Float, Float, GLfloat, GLfloat) -> [(GLfloat,GLfloat,Color3 GLfloat)]
+    colouredPoints f (scaleFactor, shiftX, shiftY, widthDensity, heightDensity) = 
+        [(shiftX +scaleFactor*x/widthDensity, shiftY + scaleFactor*y/heightDensity, intToColour $ iterations f x y 100 (widthDensity, heightDensity))|
+                        x <- [-widthDensity..widthDensity],
+                        y <- [-heightDensity..heightDensity]]
+                            where 
+                            intToColour n = Color3 (colour n) (colour 1) (colour 1)
+                                where 
+                                    colour :: Int -> GLfloat    
+                                    colour n = fromIntegral n/50    
--- src/main.hs
+++ src/main.hs
@@ -1,61 +1,18 @@
+import Fractal
+
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
 import Graphics.UI.GLUT.Window
 import Data.IORef
 
-widthDensity = 300:: GLfloat
-heightDensity = 300 :: GLfloat 
-
-data Complex = C Float Float deriving (Show,Eq)
-instance Num Complex where
-    fromInteger n = C (fromIntegral n) 0.0
-    (C x y) * (C z t) = C (z*x - y*t) (y*z + x*t)
-    (C x y) + (C z t) = C (x+z) (y+t)
-
-magnitude :: Complex -> Float
-magnitude (C x y) = sqrt(x * x + y * y)
-
-type CustomFunction = Complex -> Complex -> Complex
-functionIterates :: CustomFunction -> Complex -> [Complex]
-functionIterates f c = iterate (\z -> f z c) c
-
-type TruthCondition = Complex -> Bool 
-firstIndexOfElementSatisfyingCondition :: [Complex] -> TruthCondition -> Int
-firstIndexOfElementSatisfyingCondition [] _ = 0
-firstIndexOfElementSatisfyingCondition (x:xs) condition = if condition x then 1 else 1 + firstIndexOfElementSatisfyingCondition xs condition
-
-numberOfIterations :: CustomFunction -> Complex -> TruthCondition -> Int -> Int
-numberOfIterations f z condition maxIterations = firstIndexOfElementSatisfyingCondition (take maxIterations $ functionIterates f z) condition
-
-iterations ::  Float -> Float -> Int -> Int          
-iterations x y n = numberOfIterations f (C real imag) condition n
-                    where 
-                        f z c = z * z + c
-                        condition z = (magnitude z) > 2
-                        real = 2.0 * x / widthDensity
-                        imag = 2.0 * y / heightDensity
-
-factor = 0.7
-shiftX = -0.3
-shiftY = -0.5
-colouredPoints :: [(GLfloat,GLfloat,Color3 GLfloat)]
-colouredPoints = [ (shiftX +factor*x/widthDensity, shiftY + factor*y/heightDensity, intToColour $ iterations x y 100)|
-                    x <- [-widthDensity..widthDensity],
-                    y <- [-heightDensity..heightDensity]]
-                        where 
-                        intToColour n = Color3 (colour n) (colour 1) (colour 1)
-                            where 
-                                colour :: Int -> GLfloat    
-                                colour n = fromIntegral n/50
-
-drawFractal :: Float ->  IO ()
+drawFractal :: Int ->  IO ()
 drawFractal exponent = do
-  (progname,_) <- getArgsAndInitialize
-  initialDisplayMode $= [DoubleBuffered]
-  createWindow "Haskelbrot"
-  windowSize $= Size 600 600
-  displayCallback $= display
-  mainLoop where
+    (progname,_) <- getArgsAndInitialize
+    initialDisplayMode $= [DoubleBuffered]
+    createWindow "Haskelbrot"
+    windowSize $= Size 600 600
+    displayCallback $= display
+    mainLoop where
     display = do
         clear [ColorBuffer] 
         loadIdentity 
@@ -63,9 +20,9 @@ drawFractal exponent = do
         swapBuffers where
             drawMandelbrot =
                 renderPrimitive Points $ do
-                    mapM_ drawColoredPoint colouredPoints
+                    mapM_ drawColoredPoint $ colouredPoints f (0.7, (-0.3), (-0.5), (300::GLfloat), (300::GLfloat))
                 where
-                    f z c = z * z + c
+                    f  = (\z c -> z * z + c) :: CustomFunction
                     drawColoredPoint (x,y,c) = do
                         color c 
                         vertex $ Vertex3 x y 0
